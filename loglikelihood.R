b24bbb9359995a0f044dd72ae74a07f6c27929df
Adina Zhang <azz2107@cumc.columbia.edu>
2020-04-28 00:51:23 -0400

    Updates to loss function and AZ Newton Raphson

--- loglikelihood.R
+++ loglikelihood.R
@@ -1,4 +1,4 @@
-# Write a function that generate log-likelihood, gradient and Hessian
+# Write a function that generates loss, gradient and Hessian
 # Inputs: 
 # t - days since first case
 # y - outcome
@@ -12,35 +12,42 @@ func = function(t, y, par) {
   # Expu
   expu = exp(-b * (t - c))
   
-  # Log-likelihood
-  loglik = sum(log(a) - log(1 + expu))
+  # Loss function
+  loss = (1/2) * sum(y - (a / (1 + expu)))^2
+  
+  # First derivative matrix
+  d1loss = vector(mode = "list")
+  d1loss[[1]] = (1 / (1 + expu))
+  d1loss[[2]] = (a * (c - t) * expu) / (1 + expu)^2
+  d1loss[[3]] = (a * b * expu) / (1 + expu)^2
   
   # Gradient
-  grad = vector(mode = "numeric", 3)
+  grad = vector(mode = "numeric", length = 3)
   
-  grad[1] = 1/a
-  grad[2] = sum((1 / (1 + expu)) * (c - t) * expu)
-  grad[3] = sum((1 / (1 + expu)) * b * expu)
+  grad[[1]] = -sum(y - (a / (1 + expu)) * d1loss[[1]])
+  grad[[2]] = -sum(y - (a / (1 + expu)) * d1loss[[2]])
+  grad[[3]] = -sum(y - (a / (1 + expu)) * d1loss[[3]])
   
-  # Hessian Matrix
+  # Second derivative matrix
+  d2loss = matrix(0, 3, 3)
+  d2loss[1,1] = 0
+  d2loss[2,2] = sum(y - (a / (1 + expu)) %*% t(a * ((2 * (c - t)^2 * expu * exp(-2)) / (1 + expu)^3 - ((c - t)^2 * expu) / (1 + expu)^2)))
+  d2loss[3,3] = sum(y - (a / (1 + expu)) %*% t(a * ((2 * b^2 * expu * exp(-2)) / (1 + expu)^3 - (b^2 * expu) / (1 + expu)^2)))
+  d2loss[1,2] = sum(y - (a / (1 + expu)) %*% t((-(c - t) * expu) / (expu + 1)^2))
+  d2loss[1,3] = sum(y - (a / (1 + expu)) %*% t((-b * expu) / (expu + 1)^2))
+  d2loss[2,3] = sum(y - (a / (1 + expu)) %*% t((a * b * (c - t) * expu) / (expu + 1)^2 - (2 * a * b * (c - t) * expu * exp(-2)) / (expu + 1)^3 + (a * expu) / (expu + 1)^2))
+  d2loss[2,1] = d2loss[1,2]
+  d2loss[3,1] = d2loss[1,3]
+  d2loss[3,2] = d2loss[2,3]  
+  
+  # Hessian
   hess = matrix(0, 3, 3)
-  hess[1,1] = - 1 / a^2
-  hess[2,2] = sum(((1 + expu) * (-(t - c)^2 * expu) + (t - c)^2 * expu * exp(-2)) / (1 + expu)^2)
-  hess[3,3] = sum(((1 + expu) * (-b^2 * expu) + b^2 * expu * exp(-2)) / (1 + expu)^2)
-  
-  # Remainder of hessian matrix
   for (i in 1:3) {
     for (j in 1:3) {
-      if (i == j) {
-        next()
-      } else if (i == 1 | j == 1) {
-        hess[i,j] = 0
-      } else {
-        hess[i,j] = sum((-b * (t - c) * exp(-2) * expu - b^2 * expu * (1 + expu)) / (1 + expu)^2)
-      }
+      hess[i,j] = sum(matrix(d1loss[[i]]) %*% t(matrix(d1loss[[j]]))) - d2loss[i,j]
     }
   }
   
-  return(list(loglik = loglik, grad = grad, Hess = hess)) 
+  return(list(loss = loss, grad = grad, Hess = hess)) 
 }
 
